b96e87ece73990d64a86a09a510975687443d667
Josh McHugh <joshua.mchugh01@gmail.com>
2023-11-01 00:06:42 -0400

    Added Segment Dimension Update for Summary Component

--- src/app/pages/template/summary/summary.component.ts
+++ src/app/pages/template/summary/summary.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
+import { DimensionService } from '@shared/service/dimension.service';
+import { Layout } from '@shared/state/layout.actions';
 import { SegmentModel } from '@shared/state/layout.state';
 
 @Component({
@@ -9,11 +11,33 @@ import { SegmentModel } from '@shared/state/layout.state';
   styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent {
+  @Input() rowIndex!: number;
+  @Input() columnIndex!: number;
+  @Input() sectionIndex!: number;
   @Input() segments!: Array<SegmentModel>;
 
   summary$: Observable<string>;
 
-  constructor(private store: Store) {
+  constructor(
+    private store: Store,
+    private dimensionService: DimensionService,
+  ) {
     this.summary$ = this.store.select((state) => state.resume.summary);
+  }
+
+  public handleTrackBy(index: number): number {
+    return index;
+  }
+
+  public onResize(index: number, event: ResizeObserverEntry): void {
+    const dimension = this.dimensionService.createDimension(event.target);
+    const action = new Layout.DimensionSegmentUpdate(
+      this.rowIndex,
+      this.columnIndex,
+      this.sectionIndex,
+      index,
+      dimension,
+    );
+    this.store.dispatch(action);
   }
 }
