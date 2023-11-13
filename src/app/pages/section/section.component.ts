fce60f5ee3fb6c5ff5f5746b7e0b2edd90a7a11a
Josh McHugh <joshua.mchugh01@gmail.com>
2023-11-12 23:36:28 -0500

    Refactored the onResize observer directives into the Section component

--- src/app/pages/section/section.component.ts
+++ src/app/pages/section/section.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  NgZone,
+  OnDestroy,
+} from '@angular/core';
 import { DimensionService } from '@shared/service/dimension.service';
 import { SectionModel } from '@shared/state/layout.state';
 
@@ -7,13 +14,39 @@ import { SectionModel } from '@shared/state/layout.state';
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.scss'],
 })
-export class SectionComponent {
+export class SectionComponent implements AfterViewInit, OnDestroy {
   @Input() section!: SectionModel;
+  private observer: ResizeObserver | null = null;
 
-  public constructor(private dimensionService: DimensionService) {}
+  public constructor(
+    private readonly elementRef: ElementRef,
+    private readonly ngZone: NgZone,
+    private dimensionService: DimensionService,
+  ) {}
 
   public handleTrackBy(index: number): number {
     return index;
+  }
+
+  ngAfterViewInit() {
+    if (!this.observer) {
+      this.observer = new ResizeObserver((resizes) => {
+        for (const resize of resizes) {
+          this.ngZone.run(() => {
+            this.onResize(resize);
+          });
+        }
+      });
+    }
+    this.observer.observe(this.elementRef.nativeElement, {
+      box: 'border-box',
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.unobserve(this.elementRef.nativeElement);
+    }
   }
 
   public onResize(event: ResizeObserverEntry): void {
